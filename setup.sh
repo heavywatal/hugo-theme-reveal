6edd38a9d518350be1d37e8e6f9da31f3d3a661d
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-01-20 23:24:28 +0900

    :wrench: Make static/ if not exists

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 SRC=reveal.js
 DST=static/reveal.js
+mkdir -p static
 rsync -auvm --include='reveal.css' --include='/css' --exclude='*' $SRC/css $DST/
 rsync -auvm --exclude='*.min.js' $SRC/js $DST/
 rsync -auvm --include='notes/' --include='/plugin' --exclude='*/' $SRC/plugin $DST/
